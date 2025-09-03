47004099f7a0e260a764de2d219610eefb2bb13c
Shipovmax <shipov.max@icloud.com>
2025-09-03 21:20:03 +0300

    Сделал 5 задание

--- Первый семестр Shipov/Задание_5.py
+++ Первый семестр Shipov/Задание_5.py
@@ -1,5 +1,14 @@
 # Импортируем библиотеку
+import math
 from math import *
 
 # Вводим переменные
 n = int(input('Введите число n: '))
+
+if n <= 0:
+	print('1')
+else:
+	chislo = 1
+	for perebor in range(0, n + 1):
+		chislo += 1 / math.factorial(perebor)
+print(chislo)
